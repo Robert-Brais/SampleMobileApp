05893342b64c1a5c7f9f90aaece147ffa6455553
Robert Brais <robrais@roberts-mbp.guest.corp.microsoft.com>
2018-10-23 15:45:45 -0700

    Initial Commit

---
+++ SampleMobileApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleMobileApp
+//
+//  Created by Robert Brais on 10/23/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
